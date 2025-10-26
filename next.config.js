108e735bd32c83145ed6436c972b39bb6548e9ac
RimunAce <30347580+RimunAce@users.noreply.github.com>
2025-10-26 19:26:33 +0800

    Add beboxed.net (my own) for allowed image domain

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.ytimg.com",
       },
+      {
+        protocol: "https",
+        hostname: "beboxed.net",
+      },
     ],
   },
 };
